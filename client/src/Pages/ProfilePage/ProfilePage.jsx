4e5c351cbeb302c09114c6ae0fe501de33e2f361
Saurav Kumar <72292326+Lucifer8729@users.noreply.github.com>
2021-10-07 01:02:45 +0530

    made header placeholder, defined layout of profile page

--- client/src/Pages/ProfilePage/ProfilePage.jsx
+++ client/src/Pages/ProfilePage/ProfilePage.jsx
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { Box, Grid } from "@mui/material";
+
+import profilePic from "../../assets/sampleProfilePic.png";
+import "./styles.css";
 
 const ProfilePage = () => {
-  return <div>ProfilePage</div>;
+  const [activeTab, setActiveTab] = useState({ tab1: "", tab2: "", tab3: "" });
+  return (
+    <Box
+      sx={{
+        bgcolor: "#EEEEEE",
+        height: "100vh",
+        width: "100%",
+        position: "absolute",
+        zIndex: "-1",
+      }}
+    >
+      <Grid container mt={20} direction="row" justifyContent="center">
+        <Grid item xs={12} sm={3} sx={{ position: "relative" }}>
+          <img src={profilePic} width="100%" height="auto" alt="profilepic" />
+          <div className="profile-pic-style-div">
+            <div style={{ height: "6vw", backgroundColor: "#2BCDFF" }}></div>
+            <div style={{ height: "6vw", backgroundColor: "#3ABCFF" }}></div>
+          </div>
+        </Grid>
+        <Grid item xs={4} sx={{ bgcolor: "blue" }}>
+          about
+        </Grid>
+      </Grid>
+    </Box>
+  );
 };
 
 export default ProfilePage;
--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,9 +1,48 @@
 import React from "react";
+import { Button, ButtonGroup, Link } from "@mui/material";
 
 import "./styles.css";
 
 const Header = () => {
-  return <div className="header">Header</div>;
+  return (
+    <div className="header">
+      <ButtonGroup
+        variant="contained"
+        aria-label="outlined primary button group"
+      >
+        <Button variant="outlined">
+          <Link href="/" underline="none">
+            home
+          </Link>
+        </Button>
+        <Button variant="outlined">
+          <Link href="/profilepage" underline="none">
+            profile
+          </Link>
+        </Button>
+        <Button variant="outlined">
+          <Link href="/coursepage" underline="none">
+            course
+          </Link>
+        </Button>
+        <Button variant="outlined">
+          <Link href="/publicationpage" underline="none">
+            publication
+          </Link>
+        </Button>
+        <Button variant="outlined">
+          <Link href="/experiencepage" underline="none">
+            experience
+          </Link>
+        </Button>
+        <Button variant="outlined">
+          <Link href="/contactpage" underline="none">
+            contact
+          </Link>
+        </Button>
+      </ButtonGroup>
+    </div>
+  );
 };
 
 export default Header;

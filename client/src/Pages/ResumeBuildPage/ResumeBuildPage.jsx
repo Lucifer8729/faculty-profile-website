cc8fbbaa00d9e755e67bad5c219613c97e507138
Aniket Agarwal <aniketagarwal57@gmail.com>
2021-12-26 20:09:24 +0530

    chore: update resume page length

--- client/src/Pages/ResumeBuildPage/ResumeBuildPage.jsx
+++ client/src/Pages/ResumeBuildPage/ResumeBuildPage.jsx
@@ -1,27 +1,35 @@
-import { Paper } from '@mui/material';
-import { Box } from '@mui/system';
-import React from 'react'
+import { Box } from "@mui/system";
+import React from "react";
+
+import classes from "./resumebuildpage.module.css";
 
 const ResumeBuildPage = () => {
-    const location = window.location.pathname;
-    return (
-        <Box
+  return (
+    <Box
       sx={{
         bgcolor: "#EEEEEE",
-        height: "100vh",
+        //height: "100vh",
         width: "100%",
         position: "absolute",
         zIndex: "-1",
         display: "flex",
         flexDirection: "column",
+        //justifyContent: "space-between",
         alignItems: "center",
       }}
     >
-    <Paper sx={{mt: 5, width: "60%", display: "flex", flexDirection: "column", alignItems: "center"}}>
-        {location}
-    </Paper>
+      <div class={classes["main-page"]}>
+        <div class={classes["sub-page"]}>
+          <h3 align="center">A4 Page in Portrait.(210mm X 297mm)</h3>
+        </div>
+      </div>
+      {/* <div class={classes["main-page"]}>
+        <div class={classes["sub-page"]}>
+          <h3 align="center">A4 Page in Portrait.(210mm X 297mm)</h3>
+        </div>
+      </div> */}
     </Box>
-    )
-}
+  );
+};
 
-export default ResumeBuildPage
+export default ResumeBuildPage;
